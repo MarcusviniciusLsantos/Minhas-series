22f6e4275bf42e87a78bb1d867ce54f2f3d96694
MarcusviniciusLsantos <marcusvinici25@gmail.com>
2018-02-03 16:31:06 -0300

    Prototipo finish

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import Home from './Home'
 import NewSeries from './NewSeries'
 import Series from './Series'
+import EditSeries from './EditSeries'
 // functional-stateless component
 const About = () => <section className="intro-section"><h1>Sobre</h1></section>
 class App extends Component {
@@ -21,7 +22,7 @@ class App extends Component {
     <div className="container">
       <div className="navbar-header page-scroll">
         <a className="navbar-brand page-scroll" href="#page-top">
-            <img src="images/logo.png" height="30" />
+            <img src="/images/logo.png" height="30" />
         </a>
       </div>
 
@@ -41,7 +42,9 @@ class App extends Component {
 
     </div>
   </nav>
+  
   <Route exact path='/' component={Home} />
+  <Route path='/series-edit/:id' component={EditSeries} />
   <Route path='/series/:genre' component={Series} />
   <Route exact path='/about' component={About} />
   <Route exact path='/new' component={NewSeries} />
